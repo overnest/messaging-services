51dde73c8db10c3a211a5b49c7c7433d92c2dccf
Blake Hyde <blake@overnest.com>
2017-12-04 14:49:43 -0500

    Remove placeholder model from scaffolding.

--- messaging_service/models/__init__.py
+++ messaging_service/models/__init__.py
@@ -5,7 +5,6 @@ import zope.sqlalchemy
 
 # import or define all models here to ensure they are attached to the
 # Base.metadata prior to any initialization routines
-from .mymodel import MyModel  # flake8: noqa
 from .friend import Friend
 from .message import Message
 from .user import User
