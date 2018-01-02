2b27a080c9de767441106eb10b3f3aeecd12eb3e
Blake Hyde <blake@overnest.com>
2018-01-02 17:14:16 -0500

    Add public key to friendships; add friend detail to retrieve key for a friend

--- messaging_service/models/friend.py
+++ messaging_service/models/friend.py
@@ -5,6 +5,7 @@ from sqlalchemy import (
     Integer,
     Text,
 )
+from sqlalchemy.orm import relationship
 
 from .meta import Base
 
@@ -12,9 +13,54 @@ from .meta import Base
 class Friend(Base):
     __tablename__ = 'friends'
 
-    initiator_id = Column(Integer, ForeignKey("users.id"), nullable=False, primary_key=True,)
-    target_id = Column(Integer, ForeignKey("users.id"), nullable=False, primary_key=True,)
-    verified = Column(Boolean(name="ck_friendship_verified"), nullable=False, default=False)
-
+    initiator_id = Column(
+        Integer,
+        ForeignKey("users.id"),
+        nullable=False,
+        primary_key=True,
+    )
+    target_id = Column(
+        Integer,
+        ForeignKey("users.id"),
+        nullable=False,
+        primary_key=True,
+    )
+    verified = Column(
+        Boolean(name="ck_friendship_verified"),
+        nullable=False,
+        default=False,
+    )
     initiator_public_key = Column(Text, nullable=False)
     target_public_key = Column(Text, nullable=True)
+
+    def other_user(self, user_id):
+        """Given a ``User``, return the other user associated with this
+        friendship. Raise a ValueError if they are not associated with this
+        friendship at all.
+        """
+        other_user = None
+
+        if self.initiator_id == user_id:
+           other_user = self.target
+
+        if self.target_id == user_id:
+            other_user = self.initiator
+
+        if other_user is None:
+            raise ValueError(
+                "The user_id {} is not associated with this friendship."
+                .format(user_id)
+            )
+
+        return other_user
+
+
+    def key_for(self, user_id):
+        """Given a ``User``, return the ``*_public_key`` associated with them
+        for this friendship.
+        """
+        if user_id == self.initiator_id:
+            return self.initiator_public_key
+
+        if user_id == self.target_id:
+            return self.target_public_key
--- messaging_service/views/friends.py
+++ messaging_service/views/friends.py
@@ -2,8 +2,10 @@ import pyramid.httpexceptions as phe
 from pyramid.response import Response
 from pyramid.view import view_config
 from sqlalchemy import and_, or_
+from sqlalchemy.orm import aliased
 
 from messaging_service.models.errors import DuplicateObjectError
+from messaging_service.serialization import MissingFieldsError
 from ..authorization import validate_user
 from ..models import Friend
 from ..models import User
@@ -62,8 +64,6 @@ def friends(request):
 def create_friend(request):
     initiator = validate_user(request, query=True)
 
-    public_key = request.json_body['public_key']
-
     target = request.dbsession.query(User).filter(
         User.username == request.json_body['username']).first()
 
@@ -72,6 +72,11 @@ def create_friend(request):
 
     if target.id == initiator.id:
         raise phe.HTTPBadRequest("Cannot issue a friend request to yourself.")
+
+    public_key = request.json_body.get('public_key')
+
+    if public_key is None:
+        raise MissingFieldsError(Friend, "public_key")
 
     previous_friend_requests = request.dbsession.query(Friend).filter(
         or_(
@@ -95,6 +100,7 @@ def create_friend(request):
         elif friend_request.target_id == initiator.id:
             # Reciprocal friend request exists; verify it.
             friend_request.verified = True
+            friend_request.target_public_key = public_key
             friend_request_to_save = friend_request
             break
         else:
@@ -104,10 +110,43 @@ def create_friend(request):
         friend_request_to_save = Friend(
             initiator=initiator,
             target=target,
+            initiator_public_key=public_key,
         )
 
     request.dbsession.add(friend_request_to_save)
     return Response(status=202)
+
+
+@view_config(route_name='friend_actions', request_method='GET', renderer='json')
+def friend_detail(request):
+    user_id = validate_user(request)
+    target_username = request.matchdict['username']
+
+    initiator = aliased(User)
+    target = aliased(User)
+
+    friendship = (
+        request.dbsession.query(Friend)
+        .join(initiator, Friend.initiator_id == initiator.id)
+        .join(target, Friend.target_id == target.id)
+        .filter(
+            or_(initiator.id == user_id, target.id == user_id),
+            or_(
+                initiator.username == target_username,
+                target.username == target_username,
+            )
+        )
+    ).first()
+
+    if friendship is None:
+        raise phe.HTTPNotFound
+
+    other_user = friendship.other_user(user_id)
+
+    return {
+        'username': other_user.username,
+        'public_key': friendship.key_for(other_user.id)
+    }
 
 
 @view_config(route_name='friend_actions', request_method='POST')
